547a7aa99a127f2ac004d43ad836766dc176ae53
avnipiro <avnipiro.dev@gmail.com>
2022-12-09 23:02:55 +0000

    adding cart context and shopping icon as well as cart dropdown

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { ProductsProvider } from './contexts/products.context';
 import { UserProvider } from './contexts/user.context';
+import { CartProvider } from './contexts/cart.context';
+
 
 import './index.scss';
 const rootElement = document.getElementById('root');
@@ -12,7 +15,11 @@ render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
+        <ProductsProvider>
+          <CartProvider>
         <App />
+        </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
